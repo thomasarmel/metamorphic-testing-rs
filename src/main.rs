c2934e99bed14f9c8d32b420425f859013003791
Thomas Prévost <tprevost@smtp.i3s.unice.fr>
2024-10-17 12:00:30 +0200

    Test random uniform distribution for sha2_512

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use num_bigint::BigUint;
 use num_traits::{Num, One, Zero};
 use pbkdf2::{pbkdf2_hmac, pbkdf2_hmac_array};
 use pqc_kyber::{decapsulate, encapsulate, keypair};
-use sha2::{Sha256, Sha512};
+use sha2::{compress256, compress512, Sha256, Sha512};
 use rayon::iter::{ParallelBridge, ParallelIterator};
 use sha1::Sha1;
 use sha3::Digest;
@@ -14,17 +14,16 @@ use metamorphic_testing_rs::kyber_metamorphic::{KyberCipherBitFlipMetamorphicTes
 use metamorphic_testing_rs::MetamorphicTest;
 
 fn main() {
-    let password = b"password";
-    // number of iterations
-    let n = 60;
-    // Expected value of generated key
-    let mut salt = BigUint::zero();
-
-    num_iter::range_inclusive(BigUint::zero(), BigUint::from_str_radix("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16).unwrap())
-        .into_iter().par_bridge()
-        .for_each(|salt| {
-            let key = pbkdf2_hmac_array::<Sha1, 20>(password, &salt.to_bytes_le(), n);
-            std::io::stdout().write(&key);
-            std::io::stdout().flush();
-        });
+    let mut state = [0u64; 8];
+    let mut block = BigUint::zero();
+    loop {
+        let mut block_vec = block.to_bytes_le();
+        block_vec.resize(128, 0);
+        let block_vec: [u8; 128] = block_vec.try_into().unwrap();
+        compress512(&mut state, &[block_vec.into()]);
+        let state_u8: [u8; 64] = unsafe {std::mem::transmute(state) };
+        std::io::stdout().write(&state_u8);
+        std::io::stdout().flush();
+        block += BigUint::one();
+    }
 }
