942ed4c7e15e1a025a187c627f3da9f24ece7d9b
Tom Gouville <tom.gouville@protonmail.com>
2024-10-18 00:19:19 +0200

    rewriting a cleaner API

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,60 @@
 pub mod hash_metamorphic;
+
+use std::{ops::Deref, sync::Arc};
+
 use rayon::prelude::*;
 
-pub trait MetamorphicTest {
-    type Input: std::fmt::Debug;
-    type Output: std::fmt::Debug;
-    type State;
+pub struct MetamorphicTestRunner<State, Input, Output> {
+    /// Generate an input from a size
+    gen_input: fn(usize) -> Input,
+    /// Generate an initial state
+    gen_state: fn() -> State,
+    /// Call the function on input and state and return an output
+    call: fn(State, &Input) -> Output,
+    /// Check if the output match the condition
+    check: fn(&Output, &Output) -> bool,
+}
 
-    fn gen_input(size: usize) -> Self::Input;
+impl<
+        State: Clone + Send,
+        Input: std::fmt::Debug + Send + Clone,
+        Output: std::fmt::Debug + Send + Clone,
+    > MetamorphicTestRunner<State, Input, Output>
+{
+    fn new(
+        gen_input: fn(usize) -> Input,
+        gen_state: fn() -> State,
+        call: fn(State, &Input) -> Output,
+        check: fn(&Output, &Output) -> bool,
+    ) -> Self {
+        Self {
+            gen_input,
+            gen_state,
+            call,
+            check,
+        }
+    }
 
-    fn gen_state() -> Self::State;
-
-    fn output_check(
-        output: &Self::Output,
-        reference_output: &Self::Output,
-        should_be_equal: bool,
-    ) -> bool;
-
-    fn call(state: Self::State, input: &Self::Input) -> Self::Output;
-
-    fn convert_output_as_u8(output: &Self::Output) -> Vec<u8>;
-
-    fn run_mutator<T: Mutator<Self::Input, Self::State>>(
-        reference_output: &Self::Output,
-        mutator: &mut T,
+    fn run_mutator<T: Mutator<Input, State>>(
+        &self,
+        initial_state: &State,
+        initial_input: &Input,
+        reference_output: &Output,
+        mutator: &T,
+        max_input_size: usize,
         collect_outputs: bool,
-    ) -> (
-        Option<Vec<(Self::Input, Self::Output)>>,
-        Option<Vec<Vec<u8>>>,
-    ) {
+    ) -> (Option<Vec<(Input, Output)>>, Option<Vec<Output>>) {
         let mut collected_outputs = vec![];
         let mut collected_errors = vec![];
-        let should_be_equal = mutator.should_be_equal();
-        for (state, input) in mutator {
-            let new_output = Self::call(state, &input);
+        for size in 1..max_input_size {
+            let (mutated_state, mutated_input) =
+                mutator.mutate_input(initial_input, initial_state, size);
+            let new_output = (self.call)(mutated_state, &mutated_input);
             if collect_outputs {
-                collected_outputs.push(Self::convert_output_as_u8(&new_output));
+                collected_outputs.push(new_output.clone());
             }
-            if !Self::output_check(reference_output, &new_output, should_be_equal) {
-                collected_errors.push((input, new_output));
+            if !(self.check)(&new_output, reference_output) {
+                collected_errors.push((mutated_input, new_output));
             }
         }
 
@@ -56,31 +73,60 @@ pub trait MetamorphicTest {
         (errors, outputs)
     }
 
-    fn run_test<T: Mutator<Self::Input, Self::State>>(max_size: usize, mutator: &mut T) -> bool {
-        for s in 1..max_size {
-            let input = Self::gen_input(s);
-            println!("Running bit inclusion with {} bytes input size", s);
-            let ref_output = Self::call(Self::gen_state(), &input);
-            mutator.initial_input(input, Self::gen_state(), s * 8);
+    pub fn run_test<T: Mutator<Input, State> + Send + Sync>(
+        &self,
+        max_size: usize,
+        test_name: &str,
+        lib_name: &str,
+        mutator: T,
+    )  {
+        let shared_mutator = Arc::new(mutator.clone());
+        let number_of_errors :usize = (1..max_size)
+            .into_par_iter()
+            .map(|size| {
+                let input = (self.gen_input)(size);
+                println!(
+                    "[{}] Running {} with {} bytes input size",
+                    lib_name, test_name, size
+                );
+                let new_mutator = Arc::clone(&shared_mutator);
+                let ref_output = (self.call)((self.gen_state)(), &input);
+                let (errors, _) = self.run_mutator(
+                    &(self.gen_state)(),
+                    &input,
+                    &ref_output,
+                    new_mutator.deref(),
+                    size,
+                    false,
+                );
 
-            let (res, _) = Self::run_mutator(&ref_output, mutator, false);
+                match errors {
+                    Some(err) =>  {
+                        for e in &err {
+                            println!(
+                                "[{}] ! ERROR ! Running {} on size {} : reference input {:?}, reference output {:?}, input {:?}, output {:?}",
+                                lib_name, test_name, size, input, ref_output, e.0, e.1
+                            );
+                            
+                        }
+                        err.len()
+                    },
+                    None => 0,
+                }
+            }).sum();
 
-            if let Some(x) = res {
-                println!("Found errors : {:?}", &x);
-                return false;
-            }
-        }
-
-        true
+                println!(
+                    "[{}] !SUMMARY ! {} with 1-{} bytes : found {} errors",
+                    lib_name, test_name, max_size, number_of_errors
+                );
+        
     }
-
-    fn run_tests(max_size: usize);
 }
 
 pub trait HashMetamorphic {
-    type Input: std::fmt::Debug;
-    type Output: std::fmt::Debug;
-    type State;
+    type Input: std::fmt::Debug + Clone + Send;
+    type Output: std::fmt::Debug + Clone + Send + PartialEq;
+    type State: Clone + Send;
 
     const LIBNAME: &str;
 
@@ -106,109 +152,39 @@ pub trait HashMetamorphic {
     fn compare_output(initial_output: &Self::Output, output: &Self::Output) -> bool;
     fn hash(state: Self::State, input: &Self::Input) -> Self::Output;
 
-    fn bit_inclusion_mutator(
-        input: Self::Input,
-        state: Self::State,
-        max_index: usize,
-    ) -> BitInclusionMutator<Self::Input, Self::State> {
-        BitInclusionMutator::new(
-            input,
-            state,
-            max_index,
-            Self::input_as_u8,
-            Self::u8_as_input,
-        )
+    fn bit_inclusion_test(max_size: usize) {
+        let mutator = BitInclusionMutator::new(Self::input_as_u8, Self::u8_as_input);
+        let runner = MetamorphicTestRunner::new(
+            Self::gen_input,
+            Self::initial_state,
+            Self::hash,
+            |reference_output, output| reference_output != output,
+        );
+        runner.run_test(max_size, "Bit Inclusion", Self::LIBNAME, mutator);
     }
 
-    fn update_mutator(
-        input: Self::Input,
-        state: Self::State,
-        max_index: usize,
-    ) -> UpdateMutator<Self::Input, Self::State> {
-        UpdateMutator::new(
-            input,
-            state,
-            max_index,
-            Self::input_as_u8,
-            Self::hash_update,
-        )
-    }
-}
+    fn update_hash_test(max_size: usize) {
+        let mutator = UpdateMutator::new(Self::input_as_u8, Self::hash_update);
+        let runner = MetamorphicTestRunner::new(
+            Self::gen_input,
+            Self::initial_state,
+            Self::hash,
+            |reference_output, output| reference_output == output,
+        );
 
-impl<T: HashMetamorphic> MetamorphicTest for T {
-    type Input = T::Input;
-    type Output = T::Output;
-    type State = T::State;
-
-    fn output_check(
-        output: &Self::Output,
-        reference_output: &Self::Output,
-        should_be_equal: bool,
-    ) -> bool {
-        let out = T::output_as_u8(output);
-        let ref_out = T::output_as_u8(reference_output);
-        (out == ref_out) == should_be_equal
-    }
-
-    fn call(state: Self::State, input: &Self::Input) -> Self::Output {
-        T::hash(state, input)
-    }
-
-    fn convert_output_as_u8(output: &Self::Output) -> Vec<u8> {
-        T::output_as_u8(output)
+        runner.run_test(max_size, "Update Hash", Self::LIBNAME, mutator);
     }
 
     fn run_tests(max_size: usize) {
-        // Bit inclusion test
-        (1..max_size).into_par_iter().for_each(|s| {
-            let input = Self::gen_input(s);
-            println!(
-                "[{}] Running bit inclusion with {} bytes input size",
-                Self::LIBNAME,
-                s
-            );
-            let ref_output = T::call(Self::initial_state(), &input);
-            let mut bit_inclusion_mutator =
-                Self::bit_inclusion_mutator(input, Self::initial_state(), s * 8);
-
-            let (res, _) = Self::run_mutator(&ref_output, &mut bit_inclusion_mutator, false);
-
-            if let Some(x) = res {
-                println!("Found errors : {:?}", &x);
-            }
-        });
-
-        // Update test
-        (1..max_size).into_par_iter().for_each(|s| {
-            let input = Self::gen_input(s);
-            println!(
-                "[{}] Running update test with {} bytes input size",
-                Self::LIBNAME,
-                s
-            );
-            let ref_output = T::call(Self::initial_state(), &input);
-            let mut update_mutator = Self::update_mutator(input, Self::initial_state(), s);
-
-            let (res, _) = Self::run_mutator(&ref_output, &mut update_mutator, false);
-
-            if let Some(x) = res {
-                println!("Found errors : {:?}", &x);
-            }
-        });
-    }
-
-    fn gen_input(size: usize) -> Self::Input {
-        T::gen_input(size)
-    }
-
-    fn gen_state() -> Self::State {
-        T::initial_state()
+        Self::bit_inclusion_test(max_size);
+        Self::update_hash_test(max_size);
     }
 }
 
-pub trait DSSMetamorphic {
+pub trait KEMMetamorphic {
     type Input;
     type Output;
+    type State;
 
     fn gen_input(msg_size: usize) -> Self::Input;
     fn get_key_from_input_as_u8(input: Self::Input) -> Vec<u8>;
@@ -218,137 +194,63 @@ pub trait DSSMetamorphic {
     fn output_as_u8(output: Self::Output) -> Vec<u8>;
 }
 
-pub trait Mutator<I, S>: Iterator<Item = (S, I)> {
-    fn initial_input(&mut self, input: I, initial_state: S, max_index: usize);
-
-    fn mutate_input(&self) -> (S, I);
-
-    fn should_be_equal(&self) -> bool;
+pub trait Mutator<I: Clone, S: Clone>: Clone {
+    fn mutate_input(&self, input: &I, initial_state: &S, element_to_mutate: usize) -> (S, I);
 }
 
-pub struct BitInclusionMutator<I, S> {
-    input: I,
-    initial_state: S,
-    index: usize,
-    max_index: usize,
+#[derive(Clone)]
+pub struct BitInclusionMutator<I: Clone, S: Clone> {
     extract_mutable: fn(&I) -> Vec<u8>,
     include_mutated: fn(&S, &I, Vec<u8>) -> (S, I),
 }
 
-impl<I, S> BitInclusionMutator<I, S> {
+impl<I: Clone, S: Clone> BitInclusionMutator<I, S> {
     fn new(
-        input: I,
-        initial_state: S,
-        max_index: usize,
         extract_mutable: fn(&I) -> Vec<u8>,
         include_mutated: fn(&S, &I, Vec<u8>) -> (S, I),
     ) -> Self {
         Self {
-            input,
-            initial_state,
-            index: 0,
-            max_index,
             extract_mutable,
             include_mutated,
         }
     }
 }
 
-impl<I, S> Mutator<I, S> for BitInclusionMutator<I, S> {
-    fn mutate_input(&self) -> (S, I) {
-        let mut mutable_part = (self.extract_mutable)(&self.input);
-        flip_one_bit(&mut mutable_part, self.index);
-        (self.include_mutated)(&self.initial_state, &self.input, mutable_part)
-    }
-
-    fn should_be_equal(&self) -> bool {
-        false
-    }
-
-    fn initial_input(&mut self, input: I, initial_state: S, max_index: usize) {
-        self.input = input;
-        self.initial_state = initial_state;
-        self.max_index = max_index;
-        self.index = 0;
+impl<I: Clone, S: Clone> Mutator<I, S> for BitInclusionMutator<I, S> {
+    fn mutate_input(&self, input: &I, initial_state: &S, element_to_mutate: usize) -> (S, I) {
+        let mut mutable_part = (self.extract_mutable)(input);
+        flip_one_bit(&mut mutable_part, element_to_mutate);
+        (self.include_mutated)(initial_state, input, mutable_part)
     }
 }
 
-impl<I, S> Iterator for BitInclusionMutator<I, S> {
-    type Item = (S, I);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.max_index {
-            return None;
-        }
-        let res = Some(self.mutate_input());
-        self.index += 1;
-
-        res
-    }
-}
-
-pub struct UpdateMutator<I, S> {
-    input: I,
-    hasher: S,
-    index: usize,
-    max_index: usize,
+#[derive(Clone)]
+pub struct UpdateMutator<I: Clone, S: Clone> {
     extract_mutable: fn(&I) -> Vec<u8>,
     include_mutated: fn(&S, &I, &[u8], &[u8]) -> (S, I),
 }
 
-impl<I, S> UpdateMutator<I, S> {
+impl<I: Clone, S: Clone> UpdateMutator<I, S> {
     fn new(
-        input: I,
-        initial_state: S,
-        max_index: usize,
         extract_mutable: fn(&I) -> Vec<u8>,
         include_mutated: fn(&S, &I, &[u8], &[u8]) -> (S, I),
     ) -> Self {
         Self {
-            input,
-            hasher: initial_state,
-            index: 0,
-            max_index,
             extract_mutable,
             include_mutated,
         }
     }
 }
-impl<I, S> Mutator<I, S> for UpdateMutator<I, S> {
-    fn initial_input(&mut self, input: I, initial_state: S, max_index: usize) {
-        self.input = input;
-        self.hasher = initial_state;
-        self.max_index = max_index;
-        self.index = 0;
-    }
-
-    fn mutate_input(&self) -> (S, I) {
-        let mutable_part = (self.extract_mutable)(&self.input);
+impl<I: Clone, S: Clone> Mutator<I, S> for UpdateMutator<I, S> {
+    fn mutate_input(&self, input: &I, initial_state: &S, element_to_mutate: usize) -> (S, I) {
+        let mutable_part = (self.extract_mutable)(input);
         // Splitting on index
         (self.include_mutated)(
-            &self.hasher,
-            &self.input,
-            &mutable_part[0..self.index],
-            &mutable_part[self.index..],
+            initial_state,
+            input,
+            &mutable_part[0..element_to_mutate],
+            &mutable_part[element_to_mutate..],
         )
-    }
-
-    fn should_be_equal(&self) -> bool {
-        true
-    }
-}
-
-impl<I, S> Iterator for UpdateMutator<I, S> {
-    type Item = (S, I);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index >= self.max_index {
-            return None;
-        }
-        let res = Some(self.mutate_input());
-        self.index += 1;
-
-        res
     }
 }
 
@@ -364,10 +266,8 @@ pub fn set_one_bit(input: &mut [u8], idx: usize, value: bool) {
 }
 
 pub fn flip_one_bit(input: &mut [u8], idx: usize) {
-    // println!("{:?} << {}", input, idx);
     if idx >= input.len() * 8 {
         panic!("index error");
     }
     input[idx / 8] ^= 1 << (7 - (idx % 8));
-    // println!("{:?}", input);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-use metamorphic_testing_rs::{
-    hash_metamorphic::{
-        AsconMetamorphic, BeltMetamorphic, Blake2b512Metamorphic, Blake2s256Metamorphic,
-        Blake3Metamorphic, Fsb160Metamorphic, Fsb224Metamorphic, Fsb256Metamorphic,
-        Fsb384Metamorphic, Fsb512Metamorphic, Groestl224Metamorphic, Groestl256Metamorphic,
-        Groestl384Metamorphic, Groestl512Metamorphic, Jh224Metamorphic, Jh256Metamorphic,
-        Jh384Metamorphic, Jh512Metamorphic, RingSHA256Metamorphic, RingSHA384Metamorphic,
-        RingSHA512Metamorphic, RingSHA512_256Metamorphic, Ripemd128Metamorphic,
-        Ripemd160Metamorphic, Ripemd256Metamorphic, Ripemd320Metamorphic, Sha2_224Metamorphic,
-        Sha2_256Metamorphic, Sha2_384Metamorphic, Sha2_512Metamorphic, Sha2_512_224Metamorphic,
-        Sha2_512_256Metamorphic, Sha3_224Metamorphic, Sha3_256Metamorphic, Sha3_384Metamorphic,
-        Sha3_512Metamorphic, Shabal192Metamorphic, Shabal224Metamorphic, Shabal256Metamorphic,
-        Shabal384Metamorphic, Shabal512Metamorphic, Skein1024Metamorphic, Skein256Metamorphic,
-        Skein512Metamorphic, Sm3Metamorphic, TigerMetamorphic, WhirlpoolMetamorphic,
-    },
-    MetamorphicTest,
+use metamorphic_testing_rs::hash_metamorphic::{
+    AsconMetamorphic, BeltMetamorphic, Blake2b512Metamorphic, Blake2s256Metamorphic,
+    Blake3Metamorphic, Fsb160Metamorphic, Fsb224Metamorphic, Fsb256Metamorphic, Fsb384Metamorphic,
+    Fsb512Metamorphic, Groestl224Metamorphic, Groestl256Metamorphic, Groestl384Metamorphic,
+    Groestl512Metamorphic, Jh224Metamorphic, Jh256Metamorphic, Jh384Metamorphic, Jh512Metamorphic,
+    RingSHA256Metamorphic, RingSHA384Metamorphic, RingSHA512Metamorphic, RingSHA512_256Metamorphic,
+    Ripemd128Metamorphic, Ripemd160Metamorphic, Ripemd256Metamorphic, Ripemd320Metamorphic,
+    Sha2_224Metamorphic, Sha2_256Metamorphic, Sha2_384Metamorphic, Sha2_512Metamorphic,
+    Sha2_512_224Metamorphic, Sha2_512_256Metamorphic, Sha3_224Metamorphic, Sha3_256Metamorphic,
+    Sha3_384Metamorphic, Sha3_512Metamorphic, Shabal192Metamorphic, Shabal224Metamorphic,
+    Shabal256Metamorphic, Shabal384Metamorphic, Shabal512Metamorphic, Skein1024Metamorphic,
+    Skein256Metamorphic, Skein512Metamorphic, Sm3Metamorphic, TigerMetamorphic,
+    WhirlpoolMetamorphic,
 };
+use metamorphic_testing_rs::HashMetamorphic;
 
 fn main() {
     let test_size_bytes = 1024;
